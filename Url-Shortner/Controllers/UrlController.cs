02dadc6205099adc34d739233ab287ae93dca614
gaurava <gaurav.ahuja@newsprintgroup.com>
2019-10-10 17:37:03 +0530

    basic structure implemented.

---
+++ Url-Shortner/Controllers/UrlController.cs
@@ -0,0 +1,39 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Web.Http;
+using Url_Shortner.Service;
+
+namespace Url_Shortner.Controllers
+{
+   // [ControllerName("Client")]
+  //  [ApiVersion("1.0")]
+
+
+    public partial class UrlController : ApiController
+    {
+        [System.Web.Http.HttpGet]
+        [System.Web.Http.Route("api/shortme/{Url}", Name = "GetshortUrl")]
+       
+        public IHttpActionResult Get(string url)
+        {
+            #region contracts
+            if (!Regex.IsMatch(url, ""))
+            {
+                return BadRequest("url is not in correct format. ");
+            }
+            #endregion
+
+            UrlService urlService = new UrlService();
+            {
+                var shortUrl = urlService.makeShort();
+                //var urlModel = url.ToUrlDto();
+             
+                HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.Redirect, shortUrl);
+                return Ok(response);
+            }
+        }
+
+
+    }
+}
---
+++ Url-Shortner/Service/UrlService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Url_Shortner.Service
+{
+    public class UrlService 
+    {
+        public String makeShort()
+        {
+          
+
+           
+            return "www.google.com"; 
+        }
+    }
+}
