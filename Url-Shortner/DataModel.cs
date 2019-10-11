2e8ec86297162948cba6f8a4bdc788fa790d74d3
gaurava <gaurav.ahuja@newsprintgroup.com>
2019-10-11 11:46:25 +0530

    added dbset

--- Url-Shortner/DataModel.cs
+++ Url-Shortner/DataModel.cs
@@ -3,6 +3,7 @@ namespace Url_Shortner
     using System;
     using System.Data.Entity;
     using System.Linq;
+    using Url_Shortner.Models;
 
     public class DataModel : DbContext
     {
@@ -20,7 +21,7 @@ namespace Url_Shortner
         // Add a DbSet for each entity type that you want to include in your model. For more information 
         // on configuring and using a Code First model, see http://go.microsoft.com/fwlink/?LinkId=390109.
 
-        // public virtual DbSet<MyEntity> MyEntities { get; set; }
+         public virtual DbSet<UrlPair> MyEntities { get; set; }
     }
 
     //public class MyEntity
---
+++ Url-Shortner/Models/UrlPair.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Url_Shortner.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    public class UrlPair
+    {
+        [Key]
+        public int urlID { get; set; }
+               
+        public string shortURL { get; set; }
+
+        public string longURL { get; set; }
+        
+    }
+}
